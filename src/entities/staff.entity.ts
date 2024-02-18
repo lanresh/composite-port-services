908f5ddf4b40c20115cd70bcf1636a6e2731e4da
Lanreshittu <Lanre.shittu@babbangona.com>
2024-02-18 15:28:04 +0100

    Created Staff Entity

---
+++ src/entities/staff.entity.ts
@@ -0,0 +1,89 @@
+import { BaseEntity, Entity, PrimaryGeneratedColumn, PrimaryColumn, Column, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+import { Staff } from '@/interfaces/staff.interface';
+
+@Entity()
+export class StaffEntity extends BaseEntity implements Staff {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @PrimaryColumn()
+  userid: string;
+
+  @Column({ nullable: true })
+  firstname: string;
+
+  @Column({ nullable: true })
+  middlename: string;
+
+  @Column({ nullable: true })
+  lastname: string;
+
+  @Column({ nullable: true })
+  dob: string;
+
+  @Column({ nullable: true })
+  stateOfOrigin: string;
+
+  @Column({ nullable: true })
+  lga: string;
+
+  @Column({ nullable: true })
+  sex: string;
+
+  @Column({ nullable: true })
+  marital_status: string;
+
+  @Column({ nullable: true })
+  address: string;
+
+  @Column({ nullable: true })
+  home_phone: string;
+
+  @Column({ nullable: true })
+  cell_phone: string;
+
+  @Column()
+  email: string;
+
+  @Column({ nullable: true })
+  nextOfKin: string;
+
+  @Column({ nullable: true })
+  relationship: string;
+
+  @Column({ nullable: true })
+  addressOfNOK: string;
+
+  @Column({ nullable: true })
+  emailOfNOK: string;
+
+  @Column({ nullable: true })
+  phoneOfNOK: string;
+
+  @Column({ nullable: true })
+  date_employed: string;
+
+  @Column({ nullable: true })
+  deptid: string;
+
+  @Column({ nullable: true })
+  gradeid: string;
+
+  @Column({ nullable: true })
+  branchcode: string;
+
+  @Column({ nullable: true })
+  employee_status: string;
+
+  @Column()
+  role: string;
+
+  @Column()
+  staff_type: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
---
+++ src/interfaces/staff.interface.ts
@@ -0,0 +1,31 @@
+export interface Staff {
+    id: number;
+    userid: string;
+    firstname: string;
+    middlename: string;
+    lastname: string;
+    dob: string;
+    stateOfOrigin: string;
+    lga: string;
+    sex: string;
+    marital_status: string;
+    address: string;
+    home_phone: string;
+    cell_phone: string;
+    email: string;
+    nextOfKin: string;
+    relationship: string;
+    addressOfNOK: string;
+    emailOfNOK: string;
+    phoneOfNOK: string;
+    date_employed: string;
+    deptid: string;
+    gradeid: string;
+    branchcode: string;
+    employee_status: string;
+    role: string;
+    staff_type: string;}
+  
+  
+
+  
