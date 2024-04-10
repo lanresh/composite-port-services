3096f8064500fc1e3cb685d8e2249fd5f37a3df7
Timilehin Adeleye <timi.adeleye@babbangona.com>
2024-04-11 00:17:50 +0100

    implemented client images functionality

--- src/controllers/client.controller.ts
+++ src/controllers/client.controller.ts
@@ -1,6 +1,8 @@
+import { uploadToS3 } from '@/helpers/s3.helper';
 import { Client } from '@/interfaces/client.interface';
 import { ClientService } from '@/services/client.service';
 import { NextFunction, Request, Response } from 'express';
+import { MulterRequest } from '@/interfaces/multer.interface';
 
 export class ClientController {
   public clientService = new ClientService();
@@ -29,31 +31,59 @@ export class ClientController {
     try {
       const clientData: Client = req.body;
       const createClientData: Client = await this.clientService.createClient(clientData);
-      res.status(200).json({ data: createClientData, message: 'client created successfully' }); 
-  }
-    catch (error) {
+      res.status(200).json({ data: createClientData, message: 'client created successfully' });
+    } catch (error) {
       next(error);
     }
-}
+  };
 
-public updateClient = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const clientId = Number(req.params.id);
-    const clientData: Client = req.body;
-    const updateClientData: Client = await this.clientService.updateClient(clientId, clientData);
-    res.status(200).json({ data: updateClientData, message: 'client updated successfully' }); 
-}catch (error) {
-    next(error);
-  }
-}
+  public updateClient = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const clientId = Number(req.params.id);
+      const clientData: Client = req.body;
+      const updateClientData: Client = await this.clientService.updateClient(clientId, clientData);
+      res.status(200).json({ data: updateClientData, message: 'client updated successfully' });
+    } catch (error) {
+      next(error);
+    }
+  };
 
-public deleteClient = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const clientId = Number(req.params.id);
-    const deleteClientData = await this.clientService.deleteClient(clientId);
-    res.status(200).json({ data: deleteClientData, message: 'client deleted successfully' });
-  } catch (error) {
-    next(error);
-  }
-}
+  public deleteClient = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const clientId = Number(req.params.id);
+      const deleteClientData = await this.clientService.deleteClient(clientId);
+      res.status(200).json({ data: deleteClientData, message: 'client deleted successfully' });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  public uploadClientImage = async (req: MulterRequest, res: Response, next: NextFunction) => {
+    try {
+      const clientId = req.user.userid;
+      const { projectId, projectCode } = req.body;
+      const files = req.files;
+      const imageUrls = await uploadToS3(+projectId, files);
+
+      let result = [];
+      for (const image of imageUrls) {
+        const uploadImage = await this.clientService.uploadClientImage(clientId, image, projectId, projectCode);
+        result.push(uploadImage);
+      }
+
+      res.status(200).json({ data: result, message: 'client images uploaded successfully' });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  public findAllClientImagesByProjectId = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const projectId = req.params.id;
+      const clientImages = await this.clientService.findClientImagesByProjectId(projectId);
+      res.status(200).json({ data: clientImages, message: 'client images found successfully' });
+    } catch (error) {
+      next(error);
+    }
+  };
 }
