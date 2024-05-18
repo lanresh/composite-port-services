4ff47e49bcf0edd76e4377178c4f7267d2d29bfb
Timilehin Adeleye <timi.adeleye@babbangona.com>
2024-05-18 19:33:40 +0100

    modified client service

--- src/services/client.service.ts
+++ src/services/client.service.ts
@@ -42,7 +42,7 @@ export class ClientService extends Repository<ClientEntity> {
 
       const connection = getConnection();
       const query = `
-          INSERT INTO client_entity(userid, first_name, last_name, email, phone_number, mobile_number, address, state, activation_code. image, id_type, id_number, id_image)
+          INSERT INTO client_entity(userid, first_name, last_name, email, phone_number, mobile_number, address, state, activation_code, image, id_type, id_number, id_image)
           VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)
           RETURNING *
         `;
