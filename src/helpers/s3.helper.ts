a65b6377a6996e6491dd3990bb14fd00bf223c89
Timilehin Adeleye <timi.adeleye@babbangona.com>
2024-10-15 11:44:27 +0100

    added region to the s3 helper

--- src/helpers/s3.helper.ts
+++ src/helpers/s3.helper.ts
@@ -1,7 +1,10 @@
 import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
 
 export const uploadToS3 = async (reportId: number, files: any) => {
-  const s3 = new S3Client();
+  // const s3 = new S3Client();
+  const s3 = new S3Client({
+    region: process.env.AWS_REGION, // Explicitly setting the region
+  });
 
   const params = files.map((file: any) => {
     return {
@@ -21,7 +24,10 @@ export const uploadToS3 = async (reportId: number, files: any) => {
 };
 
 export const photoUpload = async (userId: string, file: any) => {
-  const s3 = new S3Client();
+  // const s3 = new S3Client();
+  const s3 = new S3Client({
+    region: process.env.AWS_REGION, // Explicitly setting the region
+  });
 
   const param = {
     Bucket: process.env.AWS_BUCKET_NAME,
